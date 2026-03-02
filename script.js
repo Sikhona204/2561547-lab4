d4a34a7c892643ef442ecfe496149e3ec84ccd87
Sikhona204 <sikhona204@gmail.com>
2026-03-02 16:37:43 +0200

    updated files

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function searchCountry(countryName) {
 
     try {
         
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}? FullText=true`);
         if (!response.ok) throw new Error('Country not found');
         const data = await response.json();
         const country = data[0];
